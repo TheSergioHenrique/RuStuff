65eec03bf4b5cd8ebe444b1bc98b3a09c0a5232f
Shiina <sergio5203@gmail.com>
2024-02-16 09:20:08 -0300

    guessing game finished.

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,5 +1,6 @@
 //PRELUDE
 use std::io;//io comes from the standard library.
+use std::cmp::Ordering;
 use rand::Rng;//pack from crates.io
 
 
@@ -9,17 +10,32 @@ fn main() {
     let secret_number = rand::thread_rng().gen_range(1..=100);
 
     println!("The secret number is: {secret_number}");
+    
+    loop{
+        println!("Please input your guess!");
 
-    println!("Please input your guess!");
-
-    let mut guess = String::new(); //variable are imutable by default. So, we add the mut
+        let mut guess = String::new(); //variable are imutable by default. So, we add the mut
                                    //sufix. String::new generates a new String instance directly
                                    //from STDlib.
 
-    io::stdin()
-        .read_line(&mut guess) //searches what is in the adress of mut guess at the moment.
-        .expect("Failed to read the line...");//readline is expected to send back a result value. Result is an Enum, which can be in multiple states, so to make the code safer, we need to put the .expect in the end of a call.
+        io::stdin()
+            .read_line(&mut guess) //searches what is in the adress of mut guess at the moment.
+            .expect("Failed to read the line...");//readline is expected to send back a result value. Result is an Enum, which can be in multiple states, so to make the code safer, we need to put the .expect in the end of a call.
+    
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+    
+        println!("You guessed: {guess}");
 
-    println!("You guessed: {guess}");
+        match guess.cmp(&secret_number){
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater =>println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
 }
-
